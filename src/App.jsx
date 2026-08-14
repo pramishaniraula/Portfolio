7494ccc0a0fc05756d369f819d7bfe6eee5e6737
pramishaniraula <pramisha.niraula24@apexcollege.edu.np>
2026-08-14 17:50:10 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Section from "./components/Section";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+    <Navbar />
+      <Hero />
+      <Section />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
